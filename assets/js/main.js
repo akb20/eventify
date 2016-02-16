7fed7fde61b7d4618d0ffd2d18d80cd7d1501fa3
Aaron <aaronkbetts@gmail.com>
2016-02-16 23:16:20 +0800

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(function() {
+  $(".typed").typed ({
+    strings: ["Brand Ambassadors^1000", "Street Teams^1000", "Guerilla Marketing Campaigns^1000"],
+    typeSpeed: 20,
+    loop: true
+  });
+});
